f434cef349778a2f38f10d991c5c6a1e6af437c8
Charles Augis3git config --global user.name Charles <charles.augis@ece-france.com>
2014-03-14 11:29:11 +0000

    modification

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SMAP::Application.config.secret_key_base = '97e6f19d2aa9149bffd8363cc4a93f2f2daeade970bae88877dbf174868ceeee5f5d71cc7a76c55bd947392722905197afa85d813e7ce9aaa3f66bb5369abde4'
