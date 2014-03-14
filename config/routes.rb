adb61dc2f0f3351b38a8209c81f1edd39fd9f2b9
Charles Augis3git config --global user.name Charles <charles.augis@ece-france.com>
2014-03-14 15:20:16 +0000

    avancements mise en page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SMAP::Application.routes.draw do
+root "pages#home"
+get "about" => "pages#about"
+get "offre" => "pages#offre"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
